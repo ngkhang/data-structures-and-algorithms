74e0ae1a38b656b477cc2f0b1306a1157dd9ffa5
Khang Nguyen <ngkhang0220@gmail.com>
2024-06-01 16:04:17 +0700

    update: implement and test array

--- src/data-structures/array/Array.js
+++ src/data-structures/array/Array.js
@@ -1,57 +1,132 @@
+/**
+ * Array
+ *
+ * @constructor
+ */
 class MyArray {
   constructor() {
     this.length = 0;
     this.data = {};
   }
 
-  // Access
+  /**
+   * Get item by index
+   *
+   * @param {number} index - The index of item
+   * @return {any | undefined} Return the value of element in array, and undefined otherwise.
+   */
   getByIndex = (index) => this.data[index];
 
-  // Insert at the begin
-  unshift = (...values) => {
-    let size = values.length;
+  /**
+   * Inserts new elements at the start
+   *
+   * @param {...any} items - Elements to insert at the start
+   * @return {number} Return the new length of the array
+   */
+  unshift = (...items) => {
+    let size = items.length;
     for (let step = this.length + size - 1; step >= 0; step--) {
-      this.data[step] = step - size < 0 ? values[step] : this.data[step - size];
+      this.data[step] = step - size < 0 ? items[step] : this.data[step - size];
     }
     this.length += size;
     return this.length;
-  }
+  };
 
-  // Insert at index
-  splice = (start = 0, delCount = 0, ...values) => {
+  /**
+   * Appends new elements to the end
+   *
+   * @param {...any} items - New elements to add to the array.
+   * @return {number} Returns the new length of the array.
+   */
+  push = (...items) => {
+    for (const value of items) {
+      this.data[this.length] = value;
+      this.length++;
+    }
+    return this.length;
+  };
+
+  /**
+   * Removes the last element from an array
+   *
+   * @return {any | undefined} If the array is empty, undefined is returned and the array is not modified.
+   */
+  pop = () => {
+    if (!this.length) return undefined;
+    let lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  };
+
+  /**
+   * Removes the first element from an array and returns it.
+   *
+   * @return {any | undefined} If the array is empty, undefined is returned and the array is not modified.
+   */
+  shift = () => {
+    if (!this.length) return undefined;
+
+    let firstItem = this.data[0];
+    for (let step = 0; step < this.length; step++) {
+      this.data[step] = this.data[step + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+    return firstItem;
+  };
+
+  /**
+   * Removes elements from an array and, if necessary, inserts new elements in their place,
+   *
+   * @param {number} start - he zero-based location in the array from which to start removing elements
+   * @param {number} deleteCount - The number of elements to remove.
+   * @param {...any} items - New elements need insert.
+   * @return {Array} Return the deleted elements.
+   */
+  splice = (start = 0, deleteCount = 0, ...items) => {
     let itemsDel = new MyArray();
-    if (delCount) {
-      let count = delCount;
-      // Delete
+
+    if (deleteCount) {
+      let count = deleteCount;
       for (let step = start; step < this.length; step++) {
         let itemRemove = this.data[step];
         if (count > 0) {
           itemsDel.push(itemRemove);
           count--;
         }
-        this.data[step] = this.data[step + delCount];
+        this.data[step] = this.data[step + deleteCount];
       }
-      for (let step = this.length - 1; step >= this.length - delCount; step--) {
+      for (
+        let step = this.length - 1;
+        step >= this.length - deleteCount;
+        step--
+      ) {
         delete this.data[step];
       }
-      this.length -= delCount;
+      this.length -= deleteCount;
     }
-    if (values.length) {
-      let size = values.length;
+    if (items.length) {
+      let size = items.length;
 
       for (let step = this.length + size - 1; step > start; step--) {
         this.data[step] = this.data[step - size];
       }
       for (let step = 0; step < size; step++) {
-        this.data[start + step] = values[step];
+        this.data[start + step] = items[step];
       }
       this.length += size;
     }
     return itemsDel;
-  }
+  };
 
-  // Delete value by index
-  delByIndex = (index) => {
+  /**
+   * Delete item by Index
+   *
+   * @param {number} index - The index of item in array
+   * @return {any | undefined} Return item remove.
+   */
+  delItemByIndex = (index) => {
     if (index < 0 || index > this.length - 1) return undefined;
 
     let itemDel = this.data[index];
@@ -62,26 +137,101 @@ class MyArray {
     delete this.data[this.length - 1];
     this.length--;
     return itemDel;
-  }
+  };
 
-  // Insert at the end
-  push = (...values) => {
-    for (const value of values) {
-      this.data[this.length] = value;
-      this.length++;
-    };
-    return this.length;
-  }
-
-  // Convert values to string
+  /**
+   * Convert values to string
+   *
+   * @return {string} Returns a string representation of an array.
+   */
   toString = () => {
-    let output = '';
+    if (!this.length) return "";
+
+    let output = "";
     for (let index = 0; index < this.length; index++) {
-      const ele = this.data[index];
-      output += `${ele},`
+      const value = this.data[index];
+      if (index === this.length - 1) {
+        output += `${value}`;
+        return output;
+      }
+      output += `${value},`;
     }
-    return output.trim().replace(/,?$/, '');
-  }
+    return output;
+  };
+
+  /**
+   * Copy of a section of an array
+   *
+   * @param {number} start - The beginning index of the specified portion of the array (default: 0)
+   * @param {number} end - The end index of the specified portion of the array (default: length of array)
+   * @return {Array} Returns a copy of a section of an array.
+   */
+  slice = (start = 0, end = this.length) => {
+    let subArr = new MyArray();
+
+    if (!this.length) return subArr;
+
+    if (start === undefined) start = 0;
+    else if (start < 0) start = this.length + start;
+
+    if (end < 0) end = this.length + end;
+
+    for (let index = start; index < end; index++) {
+      subArr.push(this.data[index]);
+    }
+    return subArr;
+  };
+
+  /**
+   * Adds all the elements of an array into a string, separated by the specified separator string.
+   *
+   * @param {string} separator - A string used to separate one element of the array
+   * @return {string} A string of all the elements are separated with a comma.
+   */
+  join = (separator = "") => {
+    if (this.length === 0) return "";
+    else if (this.length === 1) return `${this.data[0]}`;
+
+    let str = this.data[0];
+    for (let index = 1; index < this.length; index++) {
+      str += `${separator}${this.data[index]}`;
+    }
+    return str;
+  };
+
+  /**
+   * Determines whether an array includes a certain element
+   *
+   * @param {any} searchElement - The element to search for.
+   * @param {number} fromIndex - The position in this array at which to begin searching for searchElement.
+   * @return {Boolean} Return true or false as appropriate.
+   */
+  includes = (searchElement, fromIndex = 0) => {
+    if (!this.length) return false;
+
+    for (let key = fromIndex; key < this.length; key++) {
+      if (this.data[key] === searchElement) return true;
+    }
+    return false;
+  };
+
+  /**
+   *  Returns the index of the first occurrence of a value in an array.
+   *
+   * @param {any} searchElement - The value to locate in the array.
+   * @param {number} fromIndex - The array index at which to begin the search, default 0
+   */
+  indexOf = (searchElement, fromIndex = 0) => {
+    if (!this.length) return -1;
+
+    for (let index = fromIndex; index < this.length; index++) {
+      if (this.data[index] === searchElement) return index;
+    }
+    return -1;
+  };
 }
+
+let a = [];
+console.log(a.slice());
 
 export default MyArray;
--- src/data-structures/array/__test__/Array.test.js
+++ src/data-structures/array/__test__/Array.test.js
@@ -1,20 +1,19 @@
-import { expect, it } from 'vitest';
+import { expect, it } from "vitest";
 
-import MyArray from '../Array';
-
+import MyArray from "../Array";
 
 // Testing
-describe('Array', () => {
-  it('should create empty array', () => {
+describe("Array", () => {
+  it("should create empty array", () => {
     const newArr = new MyArray();
-    expect(newArr.toString()).toBe('');
+    expect(newArr.toString()).toBe("");
     expect(newArr.length).toBe(0);
   });
 
-  it('get value by index', () => {
+  it("should get value by index", () => {
     const newArr = new MyArray();
     let values1 = newArr.getByIndex(-1000);
-    expect(values1).toBe(undefined);
+    expect(values1).toBeUndefined();
 
     newArr.push(1, 2, 3);
     let values2 = newArr.getByIndex(0);
@@ -23,58 +22,106 @@ describe('Array', () => {
     expect(values3).toBe(2);
   });
 
-  it('should insert values at the end', () => {
+  it("should appends new elements to the end", () => {
     const newArr = new MyArray();
-    newArr.push(1)
-    expect(newArr.toString()).toBe('1');
+    newArr.push(1);
+    expect(newArr.toString()).toBe("1");
     expect(newArr.length).toBe(1);
-    newArr.push(2, 3, 4, 5)
-    expect(newArr.toString()).toBe('1,2,3,4,5');
+    newArr.push(2, 3, 4, 5);
+    expect(newArr.toString()).toBe("1,2,3,4,5");
     expect(newArr.length).toBe(5);
   });
 
-  it('should insert values at the begin', () => {
+  it("should inserts new elements at the start", () => {
     const newArr = new MyArray();
-    newArr.push(1, 'abc');
+    newArr.push(1, "abc");
     newArr.unshift(0);
-    expect(newArr.toString()).toBe('0,1,abc');
+    expect(newArr.toString()).toBe("0,1,abc");
     expect(newArr.unshift(-2, -1)).toBe(5);
-    expect(newArr.toString()).toBe('-2,-1,0,1,abc');
+    expect(newArr.toString()).toBe("-2,-1,0,1,abc");
   });
 
-  it('should insert values by index', () => {
+  it("should remove the last element", () => {
     const newArr = new MyArray();
-    newArr.push(1, 'abc');
-    newArr.unshift(0);
-    expect(newArr.toString()).toBe('0,1,abc');
-    expect(newArr.unshift(-2, -1)).toBe(5);
-    expect(newArr.toString()).toBe('-2,-1,0,1,abc');
+    expect(newArr.pop()).toBeUndefined();
+    newArr.push(1, 2, 3, 4);
+    expect(newArr.pop()).toBe(4);
+    expect(newArr.toString()).toBe("1,2,3");
   });
 
-  it('should delete values by index', () => {
+  it("should remove the first element", () => {
     const newArr = new MyArray();
-    newArr.push(1, 'abc', 2, 3, 4);
-    expect(newArr.delByIndex(-100)).toBe(undefined);
-    expect(newArr.delByIndex(99)).toBe(undefined);
-    expect(newArr.delByIndex(1)).toBe('abc');
-    expect(newArr.toString()).toBe('1,2,3,4');
+    expect(newArr.shift()).toBeUndefined();
+    newArr.push(1, 2, 3, 4);
+    expect(newArr.shift()).toBe(1);
+    expect(newArr.toString()).toBe("2,3,4");
   });
 
-  it('should delete/insert values by splice', () => {
+  it("should delete values by index", () => {
+    const newArr = new MyArray();
+    newArr.push(1, "abc", 2, 3, 4);
+    expect(newArr.delItemByIndex(-100)).toBeUndefined();
+    expect(newArr.delItemByIndex(99)).toBeUndefined();
+    expect(newArr.delItemByIndex(1)).toBe("abc");
+    expect(newArr.toString()).toBe("1,2,3,4");
+  });
+
+  it("should removes/or and inserts new elements", () => {
     const newArr = new MyArray();
     newArr.push(1, 2, 3, 4, 5, 6);
 
     let itemsRemove = newArr.splice(1, 2);
-    expect(itemsRemove.toString()).toBe('2,3');
+    expect(itemsRemove.toString()).toBe("2,3");
     expect(itemsRemove.length).toBe(2);
-    expect(newArr.toString()).toBe('1,4,5,6');
+    expect(newArr.toString()).toBe("1,4,5,6");
 
     newArr.splice(1, 2, 9, 10);
-    expect(newArr.toString()).toBe('1,9,10,6');
+    expect(newArr.toString()).toBe("1,9,10,6");
 
     let newLen = newArr.splice(0, 0, -1).length;
     expect(newLen).toBe(0);
-    expect(newArr.toString()).toBe('-1,1,9,10,6');
+    expect(newArr.toString()).toBe("-1,1,9,10,6");
     expect(newArr.length).toBe(5);
   });
+
+  it("should determines whether an array includes a certain element", () => {
+    const newArr = new MyArray();
+    expect(newArr.includes('1')).toBeFalsy();
+    newArr.push(1, 2, 'abc', 4);
+    expect(newArr.includes('1')).toBeFalsy();
+    expect(newArr.includes(2)).toBeTruthy();
+    expect(newArr.includes(2, 2)).toBeFalsy();
+  });
+
+  it("should returns the index of the first occurrence of a value", () => {
+    const newArr = new MyArray();
+    newArr.push(1, 2, 'abc', 4, 2);
+    expect(newArr.indexOf('1')).toBe(-1);
+    expect(newArr.indexOf(2)).toBe(1);
+    expect(newArr.indexOf(2, 2)).toBe(4);
+  })
+
+  it("should convert all the elements of an array and separated by the specified separator string", () => {
+    const newArr = new MyArray();
+    expect(newArr.join()).toBe('');
+    newArr.push(1);
+    expect(newArr.join()).toBe('1');
+    newArr.push(2, 'abc', 3);
+    expect(newArr.join('')).toBe('12abc3');
+    expect(newArr.join(', ')).toBe('1, 2, abc, 3');
+  });
+
+  it("should return a copy of a section of an array", () => {
+    const newArr = new MyArray();
+    expect(newArr.slice().length).toBe(0);
+    newArr.push(1, 2, 3, 4);
+    expect(newArr.slice().toString()).toBe('1,2,3,4');
+    expect(newArr.slice().length).toBe(4);
+    expect(newArr.slice(1,).toString()).toBe('2,3,4');
+    expect(newArr.slice(1, 2).toString()).toBe('2');
+    expect(newArr.slice(undefined, 2).toString()).toBe('1,2');
+    expect(newArr.slice(-2,).toString()).toBe('3,4');
+    expect(newArr.slice(undefined, -2).toString()).toBe('1,2');
+    expect(newArr.slice(-3, -1).toString()).toBe('2,3');
+  })
 });
