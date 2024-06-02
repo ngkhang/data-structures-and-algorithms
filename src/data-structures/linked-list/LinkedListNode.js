0aa2c41c21b7f53dd1e54c80e809c8fccb9bfdf6
Khang Nguyen <ngkhang0220@gmail.com>
2024-06-02 13:16:46 +0700

    implement and testing linked list

---
+++ src/data-structures/linked-list/LinkedListNode.js
@@ -0,0 +1,10 @@
+class LinkedListNode {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+
+  toString = (callback) => callback ? callback(this.value) : `${this.value}`;
+}
+
+export default LinkedListNode;
